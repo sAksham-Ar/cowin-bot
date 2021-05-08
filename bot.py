7a1409b657bfb4deb9cdcba773ec18a1c88af6ff
Saksham Arya <aryasaksham@gmail.com>
2021-05-08 21:42:53 +0530

    fixed some bugs

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ import os
 from time import sleep
 
 driver=webdriver.Chrome(os.getcwd()+r"\chromedriver.exe")
-driver.get("https://selfregistration.cowin.gov.in/");
+driver.get("https://selfregistration.cowin.gov.in/")
+action = webdriver.ActionChains(driver)
 input("done?")
 i=0
 while 1:
@@ -11,17 +12,23 @@ while 1:
     appointments=[link for link in links if link.get_attribute("href")!=None and "appointment" in link.get_attribute("href")]
     available=[appointment for appointment in appointments if appointment.text not in ["Booked","NA",""] and "May" not in appointment.text]
     if len(available)>0:
-        for a in available:
-            a.click()
-            c=input()
-            if c=='y':
-                exit()
+        action.move_to_element(available[0])
+        available[0].click()
+        c=input('Done?')
+        if c=='y':
+            exit()
     if i%2==0:
-        right=driver.find_element_by_class_name('right')
-        right.click()
+        try:
+            right=driver.find_element_by_class_name('right')
+            right.click()
+        except:
+            input("done?")
     else:
-        left=driver.find_element_by_class_name('left')
-        left.click()
+        try:
+            left=driver.find_element_by_class_name('left')
+            left.click()
+        except:
+            input("done?")
     i+=1
     sleep(2)
     
