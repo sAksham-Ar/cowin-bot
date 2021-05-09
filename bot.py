48f9819747db2771fc99511f523a8c8b71f8f09a
Saksham Arya <aryasaksham@gmail.com>
2021-05-09 18:21:30 +0530

    Fixed clicks getting intercepted

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 import os
 from time import sleep
+from selenium.webdriver.common.keys import Keys
 
 driver=webdriver.Chrome(os.getcwd()+r"\chromedriver.exe")
 driver.get("https://selfregistration.cowin.gov.in/")
-action = webdriver.ActionChains(driver)
 input("done?")
 i=0
 while 1:
@@ -12,7 +12,7 @@ while 1:
     appointments=[link for link in links if link.get_attribute("href")!=None and "appointment" in link.get_attribute("href")]
     available=[appointment for appointment in appointments if appointment.text not in ["Booked","NA",""] and "May" not in appointment.text]
     if len(available)>0:
-        action.move_to_element(available[0])
+        available[0].send_keys(Keys.ENTER)
         available[0].click()
         c=input('Done?')
         if c=='y':
